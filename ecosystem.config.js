413a05d7fe78f190b76c1abad6e4022775167feb
HuntBao <gzooler@gmail.com>
2016-12-29 11:23:58 +0800

    add config file

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name      : "vue",
+      script    : "server.js",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "root",
+      host : "59.111.99.56",
+      ref  : "origin/master",
+      repo : "localhost/Users/huntbao/vue-hackernews-2.0/.git",
+      path : "/home/vue",
+      "post-deploy" : "npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production"
+    },
+    dev : {
+      user : "node",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "git@github.com:repo.git",
+      path : "/var/www/development",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env dev",
+      env  : {
+        NODE_ENV: "dev"
+      }
+    }
+  }
+}
